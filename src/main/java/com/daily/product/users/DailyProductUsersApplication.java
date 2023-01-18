b2b85740b5c692a33819b89f16499ca6b57551d4
venh <conf312@naver.com>
2023-01-18 22:25:03 +0900

    initialize project

---
+++ src/main/java/com/daily/product/users/DailyProductUsersApplication.java
@@ -0,0 +1,13 @@
+package com.daily.product.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyProductUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyProductUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daily/product/users/DailyProductUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.product.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyProductUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
